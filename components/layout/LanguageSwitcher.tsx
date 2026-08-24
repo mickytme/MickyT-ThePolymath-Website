10b9665d0750e30f5eb264f9b2e8a565014f0898
mickytme <micky_ti@outlook.com>
2026-08-24 10:58:29 +0700

    Update project.

--- components/layout/LanguageSwitcher.tsx
+++ components/layout/LanguageSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useLocale } from "next-intl";
 import { usePathname, useRouter } from "@/i18n/navigation";
 
@@ -7,10 +8,12 @@ const languages = [
   {
     code: "en",
     label: "EN",
+    icon: "/icons/english.png",
   },
   {
     code: "th",
     label: "TH",
+    icon: "/icons/thai.png",
   },
 ];
 
@@ -18,6 +21,10 @@ export default function LanguageSwitcher() {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
+
+  const currentLanguage =
+    languages.find((language) => language.code === locale) ??
+    languages[0];
 
   const handleLanguageChange = (
     event: React.ChangeEvent<HTMLSelectElement>
@@ -30,11 +37,15 @@ export default function LanguageSwitcher() {
   };
 
   return (
-    <div className="relative">
-      <label htmlFor="language-switcher" className="sr-only">
+    <div className="relative flex items-center">
+      <label
+        htmlFor="language-switcher"
+        className="sr-only"
+      >
         Select language
       </label>
 
+      {/* Language selector */}
       <select
         id="language-switcher"
         value={locale}
@@ -44,8 +55,9 @@ export default function LanguageSwitcher() {
           appearance-none
           bg-transparent
           py-1
-          pr-5
+          pr-8
           text-sm
+          font-medium
           text-gray-300
           outline-none
           transition-colors
@@ -66,20 +78,27 @@ export default function LanguageSwitcher() {
         ))}
       </select>
 
-      {/* Small dropdown arrow */}
-      <span
+      {/* PNG language icon */}
+      <div
         className="
           pointer-events-none
           absolute
           right-0
           top-1/2
+          flex
           -translate-y-1/2
-          text-xs
-          text-gray-500
+          items-center
         "
       >
-        ▾
-      </span>
+        <Image
+          src={currentLanguage.icon}
+          alt=""
+          width={20}
+          height={20}
+          className="h-5 w-5 object-contain"
+          aria-hidden="true"
+        />
+      </div>
     </div>
   );
 }
